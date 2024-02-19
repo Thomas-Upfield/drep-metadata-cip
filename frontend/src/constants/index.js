fdc4049dbbd8324f26de8be8262aa6a92a8788e8
Thomas Upfield <113025685+Thomas-Upfield@users.noreply.github.com>
2024-02-19 13:50:13 +0000

    Delete frontend directory
    deleting boilerplate

--- frontend/src/constants/index.js
+++
@@ -1,6 +0,0 @@
-// This file consolidates default values and constants used throughout the application,
-// such as pagination settings (size and default page), default locale, and more.
-// These constants ensure uniform behavior and settings across different parts of the app.
-// Refer to this file to use or update default configurations for consistent application functionality.
-
-export const locales = ["en", "de"];
--- frontend/src/lib/utils.js
+++
@@ -1,3 +0,0 @@
-export const sumTestExample = (a, b) => {
-  return a + b;
-};
